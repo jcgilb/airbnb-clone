b894354db84973f4bf2e404594042fc03bf87e25
Joanna <gilbert.joannac@gmail.com>
2022-11-23 14:19:05 -0700

    seed data

--- practice-for-week-19-python-project-skeleton/app/seeds/users.py
+++ practice-for-week-19-python-project-skeleton/app/seeds/users.py
@@ -3,16 +3,78 @@ from app.models import db, User, environment, SCHEMA
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    user1 = User(
+        username='Demo', 
+        email='demo@aa.io', 
+        password='password', 
+        first_name="Demo", 
+        last_name="User", 
+        )
+    user2 = User(
+        username='marnie', email='marnie@aa.io', password='password', first_name="Marnie", 
+        last_name="User", )
+    user3 = User(
+        username='bobbie', email='bobbie@aa.io', password='password',first_name="Bobbie", 
+        last_name="User", )
+    user4 = User(
+        username='Demo', 
+        email='demo@aa.io', 
+        password='password', 
+        first_name="Demo", 
+        last_name="User", 
+        )
+    user5 = User(
+        username='Kim_user', 
+        email='kim@aa.io', 
+        password='password', 
+        first_name="Kim", 
+        last_name="User")    
+    user6 = User(
+        username='Kevin', 
+        email='kevin@aa.io', 
+        password='password', 
+        first_name="Kevin", 
+        last_name="User", 
+        )
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    user7 = User(
+        username='Micah', 
+        email='micah@aa.io', 
+        password='password', 
+        first_name="Micah", 
+        last_name="User", 
+        )
+    user8 = User(
+        username='Emma', 
+        email='emma@aa.io', 
+        password='password', 
+        first_name="Emma", 
+        last_name="User", 
+        )
+    user9 = User(
+        username='James', 
+        email='james@aa.io', 
+        password='password', 
+        first_name="James", 
+        last_name="User", 
+        )
+    user10 = User(
+        username='Anna', 
+        email='anna@aa.io', 
+        password='password', 
+        first_name="Anna", 
+        last_name="User", 
+        )                    
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.add(user3)
+    db.session.add(user4)
+    db.session.add(user5)
+    db.session.add(user6)
+    db.session.add(user7)
+    db.session.add(user8)
+    db.session.add(user9)
+    db.session.add(user10)
     db.session.commit()
 
 
