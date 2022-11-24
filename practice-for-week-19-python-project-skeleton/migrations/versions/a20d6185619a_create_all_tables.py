c3be3aec62b82e055da0d9457fe354fe9d674ed6
Joanna <gilbert.joannac@gmail.com>
2022-11-24 01:49:42 -0700

    fix migration version

--- practice-for-week-19-python-project-skeleton/migrations/versions/a20d6185619a_create_all_tables.py
+++ practice-for-week-19-python-project-skeleton/migrations/versions/a20d6185619a_create_all_tables.py
@@ -7,6 +7,9 @@ Create Date: 2022-11-23 23:03:16.713947
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
