5f7758841aaeb298e9f9f602be1a955fc51a7b79
Joanna <gilbert.joannac@gmail.com>
2022-12-01 19:43:47 -0700

    fix signup bug

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -17,7 +17,9 @@ const SignUpForm = () => {
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+      const data = await dispatch(
+        signUp(username, firstName, lastName, email, password)
+      );
       if (data) {
         setErrors(data);
       }
