517b6f70a73c83350764e1b1500919e4b486df51
Joanna <gilbert.joannac@gmail.com>
2022-12-04 22:07:50 -0700

    landing page and 404 page

--- react-app/src/components/NotFoundPage.js
+++ react-app/src/components/NotFoundPage.js
@@ -1,5 +1,20 @@
+import "./NotFound.css";
+
 const NotFoundPage = () => {
-  return <div>Not Found</div>;
+  return (
+    <>
+      <div className="not-found">
+        <div className="not-found-title">404 Page Not Found</div>
+        <div className="not-found-page">
+          <img
+            className="not-found"
+            alt="404"
+            src="../../assets/racoon-washing-cotton-candy.png"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFoundPage;
